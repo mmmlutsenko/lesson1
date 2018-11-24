259999d41e24e3989af16271e1036e34dbec6ecf
Мария Луценко <maria.lutsenko@MacBook-Pro-Maria.local>
2018-11-24 18:36:08 +0300

    my first bot during lesson1

--- price.py
+++ price.py
@@ -1,5 +1,9 @@
 def format_price(price):
     price=int(price)
-    return 'Цена:'+ str(price)+ 'руб. '
-print(format_price(56.24))
+    return 'Цена: '+ str(price)+ ' руб. '
+display_price=format_price(56.24)
+print(display_price)
+
+
+
 
